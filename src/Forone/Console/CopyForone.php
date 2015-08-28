a67eba3015edcb6a7989030390d9777ca10f6f0f
Mani Wang <mani95lisa@gmail.com>
2015-08-28 17:26:05 +0800

    fix forone:copy bug and copy Controllers

--- src/Forone/Console/CopyForone.php
+++ src/Forone/Console/CopyForone.php
@@ -37,8 +37,39 @@ class CopyForone extends Command {
      */
     public function fire()
     {
+        function copyr($source, $dest)
+        {
+            if (is_link($source)) {
+                return symlink(readlink($source), $dest);
+            }
+
+            if (is_file($source)) {
+                $copied = copy($source, $dest);
+                $content = file_get_contents($dest);
+                $content = str_replace("Forone\Admin\Controllers", "App\Http\Controllers\Forone\Admin\Controllers", $content);
+                file_put_contents($dest, $content);
+                return $copied;
+            }
+
+            if (!is_dir($dest)) {
+                mkdir($dest, 0777, true);
+            }
+
+            $dir = dir($source);
+            while (false !== $entry = $dir->read()) {
+                if ($entry == '.' || $entry == '..') {
+                    continue;
+                }
+                copyr("$source/$entry", "$dest/$entry");
+            }
+
+            $dir->close();
+            return true;
+        }
+
         if($this->confirm('Copy routes? This will override app/Http/routes.php file, please backup that first')){
             copy('vendor/forone/administrator/src/Forone/routes.php', 'app/Http/routes.php');
+            copyr('vendor/forone/administrator/src/Forone/Controllers', 'app/Http/Controllers/Forone/Admin/Controllers');
             $config = file_get_contents('config/forone.php');
             $config = str_replace("'disable_routes' => false", "'disable_routes' => true", $config);
             file_put_contents('config/forone.php', $config);
